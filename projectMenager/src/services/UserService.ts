adf3763438458bb20e5ecd54f5eb64013b2262c0
Krystian <Krystian@DESKTOP-DA1HJHA>
2024-07-05 13:29:50 +0200

    add mock user

--- projectMenager/src/services/UserService.ts
+++ projectMenager/src/services/UserService.ts
@@ -1,27 +1,28 @@
 import { User } from '../models/User'
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid'
 
 class UserService {
 	private static USERS_KEY = 'users'
-	private static  STORAGE_KEY = 'loggedInUser'
+	private static STORAGE_KEY = 'loggedInUser'
+
+	static mockUsers(): void {
+		const users: User[] = [
+			{ id: uuidv4(), firstName: 'Adam', lastName: 'aaa', role: 'admin' },
+			{ id: uuidv4(), firstName: 'Patryk', lastName: 'sas', role: 'devops' },
+			{ id: uuidv4(), firstName: 'Jan', lastName: 'sus', role: 'developer' },
+		]
+		localStorage.setItem(this.USERS_KEY, JSON.stringify(users))
+		localStorage.setItem(this.STORAGE_KEY, JSON.stringify(users[0]))
+	}
+
+	static getAllUsers(): User[] {
+		const users = localStorage.getItem(this.USERS_KEY)
+		return users ? JSON.parse(users) : []
+	}
 
 	static getLoggedInUser(): User | null {
 		const user = localStorage.getItem(this.STORAGE_KEY)
 		return user ? JSON.parse(user) : null
 	}
-
-	static setLoggedInUser(user: User): void {
-		localStorage.setItem(this.STORAGE_KEY, JSON.stringify(user))
-	}
-
-	static mockLoggedInUser(): void {
-		const mockUser: User = {
-			id: '1',
-			firstName: 'John',
-			lastName: 'Doe',
-			role:'admin'
-		}
-        this.setLoggedInUser(mockUser);
-	}
 }
-export default UserService;
+export default UserService
